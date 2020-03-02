08b26fc81867e4a451731088a201bb809eaa9b9b
Sergey Bronnikov <estetus@gmail.com>
2020-03-02 09:49:12 +0300

    Обновляем архив и авторов

--- authors.js
+++ authors.js
@@ -4,9 +4,14 @@ import authorId from './helpers/author-id';
 export default authorId([
   // post: false - Doesn't display in list
   // update: true - Will run update author with update script
+  { username: 'ainomc1', start: '02 Mar 2020', first: '1232940202559143936', post: false, update: true },
+  { username: 's_pirogov', start: '10 Feb 2020', first: '1226766705566920704', update: false },
+  { username: 'brekelov', start: '03 Feb 2020', first: '1224230188571152384', update: false },
+  { username: 'wrong_habits', start: '20 Jan 2020', first: '1219255551516954624', update: false },
+  { username: 'sqaunderhood', start: '01 Oct 2016', first: '782281027725164544', update: true },
   { username: '1101_debian', start: '05 Sep 2016', first: '772697945011548161', update: true },
-  { username: 'maxbeard12', start: '29 Aug 2016', first: '770170042369576961', update: true },
-  { username: 'asatarin', start: '20 Aug 2016', first: '766911352958963712', update: true },
-  { username: 'art_koshelev', start: '24 Jul 2016', first: '757168508819996672', update: true },
-  { username: 'estet', start: '18 Jul 2016', first: '754962466845683712', update: true },
+  { username: 'maxbeard12', start: '29 Aug 2016', first: '770170042369576961', update: false },
+  { username: 'asatarin', start: '20 Aug 2016', first: '766911352958963712', update: false },
+  { username: 'art_koshelev', start: '24 Jul 2016', first: '757168508819996672', update: false },
+  { username: 'estet', start: '18 Jul 2016', first: '754962466845683712', update: false },
 ]);
